e88c7860f37bb947088451dd2b5ea45adab7b3ba
felpureza <felppureza@gmail.com>
2021-02-04 13:42:58 -0300

    Puxando um arquivo da internet

---
+++ data.py
@@ -0,0 +1,5 @@
+import urllib.request
+
+url = 'https://datasets.saude.go.gov.br/coronavirus/casos_confirmados.csv'
+urllib.request.urlretrieve(url, 'C:/Users/03829889143/Documents/GitHub/Graphs/Graphs/datases/casos.csv')
+
