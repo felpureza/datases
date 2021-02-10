6a24b8ec7c48f6266242fe7614541677b06dad98
Felipe Pureza <55854158+felpureza@users.noreply.github.com>
2021-02-10 15:53:35 -0300

    Alterações finais da primeira versão

---
+++ app.py
@@ -0,0 +1,27 @@
+# Pacotes externos
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+
+# Pacotes internos
+from plot import fig
+
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
+app.layout = html.Div(children=[
+    html.H1(children='Hello Dash'),
+
+    html.Div(children='''
+        Dash: A web application framework for Python.
+    '''),
+
+    dcc.Graph(
+        id='example-graph',
+        figure=fig
+    )
+])
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
